b7cc9db222fcd0dd70dcb142ba7ee63f51a926e1
samwhelp <samwhelp@users.noreply.github.com>
2026-03-02 01:58:16 +0800

    fix

--- prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/Screenshot.php
+++ prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/Screenshot.php
@@ -1,5 +1,33 @@
 
 
+	<!-- ## via xfce4-screenshooter //-->
+	<keybind key="Print">
+		<action name="Execute">
+			<command>xfce4-screenshooter --fullscreen</command>
+		</action>
+	</keybind>
+
+	<keybind key="A-Print">
+		<action name="Execute">
+			<command>xfce4-screenshooter</command>
+		</action>
+	</keybind>
+
+	<keybind key="W-Print">
+		<action name="Execute">
+			<command>xfce4-screenshooter --window</command>
+		</action>
+	</keybind>
+
+	<keybind key="C-Print">
+		<action name="Execute">
+			<command>xfce4-screenshooter --region</command>
+		</action>
+	</keybind>
+
+
+<?php
+/*
 	<!-- Take a screenshot of the current window with scrot when Alt+Print are pressed -->
 	<keybind key="A-Print">
 		<action name="Execute">
@@ -13,6 +41,10 @@
 			<command>scrot</command>
 		</action>
 	</keybind>
+
+*/
+?>
+
 
 <?php
 /*
