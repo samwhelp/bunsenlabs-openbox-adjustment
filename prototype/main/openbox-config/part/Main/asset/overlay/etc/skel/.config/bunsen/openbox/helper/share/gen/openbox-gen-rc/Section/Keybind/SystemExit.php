44c74c2750c77339e4dd73312531d64656a90b6f
samwhelp <samwhelp@users.noreply.github.com>
2026-03-02 00:39:32 +0800

    fix

--- prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/SystemExit.php
+++ prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/SystemExit.php
@@ -1,13 +1,27 @@
 
 
+	<!-- ## Reconfigure //-->
+	<keybind key="A-S-c">
+		<action name="Reconfigure" />
+	</keybind>
+
+
+	<!-- ## Lock //-->
+	<keybind key="A-C-z">
+		<action name="Execute">
+			<command>bl-lock</command>
+		</action>
+	</keybind>
+
+
 	<!-- ## Restart //-->
 	<keybind key="A-S-z">
 		<action name="Execute">
 			<startupnotify>
-				<enabled>true</enabled>
+				<enabled>false</enabled>
 				<name>Exit</name>
 			</startupnotify>
-			<command>obsession-logout</command>
+			<command>bl-exit</command>
 		</action>
 	</keybind>
 
@@ -22,15 +36,15 @@
 
 	<!-- ## Logout //-->
 	<keybind key="A-S-x">
+		<action name="Execute">
+			<command>bl-exit</command>
+		</action>
+	</keybind>
+
+	<keybind key="A-C-x">
 		<action name="Exit" />
 	</keybind>
 
-	<keybind key="C-A-Delete">
+	<keybind key="A-C-Delete">
 		<action name="Exit" />
-	</keybind>
-
-
-	<!-- ## Reconfigure //-->
-	<keybind key="A-S-c">
-		<action name="Reconfigure" />
 	</keybind>
