89b225c27e1ee6ce9e541e11f71b1765f7787173
samwhelp <samwhelp@users.noreply.github.com>
2026-03-02 00:46:57 +0800

    fix

--- prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/MenuRoot.php
+++ prototype/main/openbox-config/part/Main/asset/overlay/etc/skel/.config/bunsen/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/MenuRoot.php
@@ -6,9 +6,23 @@
 		</action>
 	</keybind>
 
+<?php
+/*
 	<keybind key="Alt-F1">
 		<action name="ShowMenu">
 			<menu>root-menu</menu>
+		</action>
+	</keybind>
+*/
+?>
+
+	<keybind key="Alt-F1">
+		<action name="Execute">
+			<startupnotify>
+				<enabled>false</enabled>
+				<name>MainMenu</name>
+			</startupnotify>
+			<command>jgmenu_run</command>
 		</action>
 	</keybind>
 
